3fccdc83e847c728a21dd25d749c1fd99adce707
MrPedroGabriel123 <joaofelipechannel0@gmail.com>
2022-08-27 18:42:55 +0100

    1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Tracker;
+
+internal static class Track
+{
+    public static void Main()
+    {
+
+    }
+}
