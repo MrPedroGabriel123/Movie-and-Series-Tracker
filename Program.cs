b9e64ae96d3b63fa094345c69a87ca69e6d65a42
MrPedroGabriel123 <joaofelipechannel0@gmail.com>
2022-08-27 18:54:21 +0100

    intro

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ internal static class Track
 {
     public static void Main()
     {
-        Console.WriteLine("Welcome Movie Time, Here you can track of everything you see or have seen");
+        Console.WriteLine("Welcome Movie Time, Here you can track of everything you see or have seen!");
+        Console.ReadKey();
     }
 }
