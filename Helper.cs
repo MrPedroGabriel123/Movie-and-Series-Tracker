b44c230d90a5651e328bba0af30082ffca1a0b33
MrPedroGabriel123 <joaofelipechannel0@gmail.com>
2022-08-27 19:28:48 +0100

    added helper.cs and made the first menu

---
+++ Helper.cs
@@ -0,0 +1,50 @@
+﻿public static class Helper
+{
+    public static int PromptInt(this string question, string? description = null, int minValue = 0,
+        int maxValue = int.MaxValue, int? defaultValue = null)
+    {
+        Console.Write(
+            $"{question}{(string.IsNullOrEmpty(description) ? "" : $" ({description})")}{(defaultValue == null ? "" : $" [default {defaultValue.Value.ToString()}]")}: ");
+        int response;
+
+        string? InputOrDefault()
+        {
+            var input = Console.ReadLine();
+            return string.IsNullOrEmpty(input) && defaultValue != null ? defaultValue.ToString() : input;
+        }
+
+        while (!int.TryParse(InputOrDefault(), out response) ||
+               response < minValue ||
+               response > maxValue)
+        {
+            var range = "";
+            if (minValue != 0 || maxValue != int.MaxValue)
+            {
+                range = $" [{minValue}-{maxValue}]";
+            }
+
+            Console.Write($"Enter a valid number:{range} ");
+        }
+
+        return response;
+    }
+
+    public static string? PromptString(this string question, string? description = null, string? defaultValue = null)
+    {
+        string? InputOrDefault()
+        {
+            var input = Console.ReadLine();
+            return string.IsNullOrEmpty(input) && defaultValue != null ? defaultValue : input;
+        }
+
+        string? response;
+        do
+        {
+            Console.Write(
+                $"{question}{(string.IsNullOrEmpty(description) ? "" : $" ({description})")}{(defaultValue == null ? "" : $" [default {defaultValue}]")}: ");
+            response = InputOrDefault();
+        } while (string.IsNullOrWhiteSpace(response) && response != defaultValue);
+
+        return response;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,13 +4,19 @@ internal static class Track
 {
     public static void Main()
     {
+        Console.ForegroundColor = ConsoleColor.Blue;
         Console.WriteLine("Welcome to Movie Time, Here you can track of everything you see or have seen!");
         Console.ReadKey();
         ///Here there should be a reset on screen using Console.Clear();
         Console.Clear();
         /////Menu below
-        Console.ForegroundColor = ConsoleColor.Red;
+        Console.ForegroundColor = ConsoleColor.Green;
         Console.WriteLine("Movie Time = Active Sub");
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("1) Create a new Profile (Up to 4) ");
+        Console.WriteLine("2) Choose what profile you want to enter");
+        Console.WriteLine("3) Enter your profile");
+        var userFirstChoice = "Enter choice".PromptInt(minValue: 1, maxValue: 3);
         ///Menu code
     }
 }
