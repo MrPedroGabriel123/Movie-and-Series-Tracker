12040e8a51b1811043eb4b67e4126e3749b4665c
MrPedroGabriel123 <joaofelipechannel0@gmail.com>
2022-08-27 19:08:43 +0100

    test

--- Program.cs
+++ Program.cs
@@ -4,7 +4,13 @@ internal static class Track
 {
     public static void Main()
     {
-        Console.WriteLine("Welcome Movie Time, Here you can track of everything you see or have seen!");
+        Console.WriteLine("Welcome to Movie Time, Here you can track of everything you see or have seen!");
         Console.ReadKey();
+        ///Here there should be a reset on screen using Console.Clear();
+        Console.Clear();
+        /////Menu below
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Movie Time = Active Sub");
+        ///Menu code
     }
 }
